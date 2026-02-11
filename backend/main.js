e85abf03454aca3587c8e798a9fff8252c8781be
Joramm <joramm_delatorre@dlsu.edu.ph>
2026-02-11 20:35:16 +0800

    side navigation bar reservation-list skeleton

---
+++ backend/main.js
@@ -0,0 +1,14 @@
+let nav_toggled = false;
+
+const side_nav = document.getElementById("in-side-nav");
+
+function toggleNav() {
+    if (nav_toggled) {
+        side_nav.style.width = "0px";
+        side_nav.style.padding = "0px";
+    } else {
+        side_nav.style.width = "300px";
+        side_nav.style.padding = "20px";
+    }
+    nav_toggled = !nav_toggled;
+}

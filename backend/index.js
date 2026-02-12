cbe58dc11e9834739e979a7dd6c413d900b92978
deppy0 <dependant1b@gmail.com>
2026-02-13 07:09:07 +0800

    smoothened transitions for /

--- backend/index.js
+++ backend/index.js
@@ -4,26 +4,41 @@ const ctr_form = document.getElementById('index-forms');
 const form_reg = document.getElementById('register');
 const form_login = document.getElementById('login');
 
-btn_reg.addEventListener('click', function(e) {
+// must be strictly greater than duration for <form> transitions, see login.css
+const delay = 300;
+
+function sleep(ms) {
+	return new Promise(r => setTimeout(r, ms));
+}
+
+btn_reg.addEventListener('click', async function(e) {
 	ctr_form.style['flex-direction'] = 'row-reverse';
-	form_login.style['transform'] = 'translateX(40%) translateY(5%)';
+	form_login.style['transform'] = 'translateX(30%)';
 	form_login.style['display'] = 'none';
 	form_login.style['opacity'] = '0.0';
-	form_reg.style['transform'] = 'translateX(40%) translateY(5%)';
+	// wait for transition to finish
+	await sleep(delay);
+	form_reg.style['transform'] = 'translateX(30%)';
 	form_reg.style['transform'] = 'none';
 	form_reg.style['display'] = 'flex';
 	form_reg.style['opacity'] = '1.0';
+	// wait for transition to finish
+	await sleep(delay);
 	void ctr_form.offsetWidth;
 });
 
-btn_ret_login.addEventListener('click', function(e) {
+btn_ret_login.addEventListener('click', async function(e) {
 	ctr_form.style['flex-direction'] = 'row';
-	form_reg.style['transform'] = 'translateX(40%) translateY(5%)';
+	form_reg.style['transform'] = 'translateX(30%)';
 	form_reg.style['display'] = "none";
 	form_reg.style['opacity'] = "0.0";
-	form_login.style['transform'] = 'translateX(40%) translateY(5%)';
+	// wait for transition to finish
+	await sleep(delay);
+	form_login.style['transform'] = 'translateX(30%)';
 	form_login.style['transform'] = 'none';
 	form_login.style['display'] = "flex";
 	form_login.style['opacity'] = "1.0";
+	// wait for transition to finish
+	await sleep(delay);
 	void ctr_form.offsetWidth;
 });

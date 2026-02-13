2285f969dbe36465dc3bfaaa1d34d773cde18a0e
deppy0 <dependant1b@gmail.com>
2026-02-13 19:18:50 +0800

    add redirection to dashboard menu options

---
+++ backend/dashboard.js
@@ -0,0 +1,36 @@
+const mopt_reserve = document.getElementById('menu-option-reserve');
+const mopt_reservation = document.getElementById('menu-option-reservation');
+const mopt_account = document.getElementById('menu-option-account');
+
+async function isServer() {
+	try {
+		const r = await fetch('http://127.0.0.1:2222', {
+			method: 'HEAD',
+			mode: 'no-cors'
+		});
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+mopt_reserve.addEventListener('click', async function() {
+	if (await isServer())
+		window.location.href = '/reserve_seats';
+	else
+		window.location.href = './DLSU-buildings.html';
+});
+
+mopt_reservation.addEventListener('click', async function() {
+	if (await isServer())
+		window.location.href = '/see_reservations';
+	else
+		window.location.href = './reservation-list.html';
+});
+
+mopt_account.addEventListener('click', async function() {
+	if (await isServer())
+		window.location.href = '/account_settings';
+	else
+		window.location.href = './account-settings.html';
+});

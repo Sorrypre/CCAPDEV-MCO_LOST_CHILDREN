db4416218a762601b53ee24b75514d92aadfebcb
deppy0 <dependant1b@gmail.com>
2026-02-13 16:29:29 +0800

    added redirects

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -8,6 +8,14 @@ import { fileURLToPath } from 'url';
 import login from './login.mjs';
 // ...
 
+// q = reQuest
+// r = Response
+// s = Success
+function checkUser(q, r, s) {
+	// For now, pasok lang nang pasok
+	s();
+}
+
 const port = 2222;
 const xj = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -15,12 +23,37 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 xj.use(express.urlencoded({ extended: true }));
 xj.use(express.json());
 xj.use(express.static(path.join(__dirname, '../')));
-/*xj.use(express.static(__dirname));*/
 xj.use(cors());
 xj.use(login);
+
 xj.get('/', (q, r) => {
 	r.redirect('/frontend/pages/');
 });
+
+xj.get('/dashboard', checkUser, (q, r) => {
+	r.redirect('/frontend/pages/dashboard.html');
+});
+
+xj.get('/reserve_seats', checkUser, (q, r) => {
+	r.redirect('/frontend/pages/DLSU-buildings.html');
+});
+
+xj.get('/see_reservations', checkUser, (q, r) => {
+	r.redirect('/frontend/pages/reservation-list.html');
+});
+
+xj.get('/account_settings', checkUser, (q, r) => {
+	r.redirect('/frontend/pages/account-settings.html');
+});
+
+xj.get('/contact_us', checkUser, (q, r) => {
+	r.redirect('/frontend/pages/about.html');
+});
+
+xj.get('/lou', checkUser, (q, r) => {
+	r.send('under construction');
+});
+
 xj.listen(port, '127.0.0.1', () => {
 	console.log('server is now on listen @ port ' + port);
 });
---
+++ backend/sidebar.js
@@ -0,0 +1,60 @@
+const redir_dashboard = document.getElementById('redir-dashboard');
+const redir_reserve_seats = document.getElementById('redir-reserve-seats');
+const redir_see_reservations = document.getElementById('redir-see-reservations');
+const redir_account_settings = document.getElementById('redir-account-settings');
+const redir_contact_us = document.getElementById('redir-contact-us');
+const redir_log_out = document.getElementById('redir-log-out');
+
+async function isServer() {
+	try {
+		const r = await fetch('http://127.0.0.1:2222', {
+			method: 'HEAD',
+			mode: 'no-cors'
+		});
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+redir_dashboard.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/dashboard';
+	else
+		window.location.href = './dashboard.html';
+});
+
+redir_reserve_seats.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/reserve_seats';
+	else
+		window.location.href = './DLSU-buildings.html';
+});
+
+redir_see_reservations.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/see_reservations';
+	else
+		window.location.href = './reservation-list.html';
+});
+
+redir_account_settings.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/account_settings';
+	else
+		window.location.href = './account-settings.html';
+});
+
+redir_contact_us.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/contact_us';
+	else
+		window.location.href = './about.html';
+});
+
+redir_log_out.addEventListener('click', async function(e) {
+	if (await isServer())
+		window.location.href = '/lou';
+	else
+		return;
+});

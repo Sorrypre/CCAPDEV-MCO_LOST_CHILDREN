50007608e58861587d5266d6b9bebbdbbd5141b6
deppy0 <dependant1b@gmail.com>
2026-02-14 20:46:40 +0800

    fixed collapse.js to aid with the folding-back animation

--- backend/collapse.js
+++ backend/collapse.js
@@ -1,19 +1,40 @@
-const collapsibles = document.getElementsByClassName('collapsible-button');
-for (let i = 0; i < collapsibles.length; i++) {
-	collapsibles[i].addEventListener('click', function(e) {
-		function nextSiblingOf(elem, css_match) {
-			let sibling = elem.nextElementSibling;
-			while (sibling)
-				if (sibling.matches(css_match))
-					return sibling;
-				else
-					sibling = sibling.nextElementSibling;
-			return null;
-		}
-		const nso = nextSiblingOf(e.target, '.collapsible-target');
-		if (!nso)
-			return;
-		nso.style['height'] = nso.matches('.folded') ? nso.scrollHeight + 'px' : '0';
-		nso.classList.toggle('folded');
+async function sleep(ms) {
+	return new Promise(function(e) {
+		setTimeout(e, ms);
 	});
+}
+
+function nextSiblingOf(elem, css_match) {
+	let sibling = elem.nextElementSibling;
+	while (sibling)
+		if (sibling.matches(css_match))
+			return sibling;
+		else
+			sibling = sibling.nextElementSibling;
+	return null;
+}
+
+const collapsibles = document.getElementsByClassName('collapsible-button');
+if (collapsibles) {
+	for (let i = 0; i < collapsibles.length; i++) {
+		collapsibles[i].addEventListener('click', async function(e) {
+			if (e.target.hasAttribute('disabled'))
+				return;
+			e.target.setAttribute('disabled', '');
+			const nso = nextSiblingOf(e.target, '.collapsible-target');
+			if (!nso)
+				return;
+			if (!nso.matches('.folded')) {
+				nso.style['height'] = nso.scrollHeight + 'px';
+				await sleep(200);
+			}
+			nso.style['height'] = nso.matches('.folded') ? nso.scrollHeight + 'px' : '0';
+			nso.classList.toggle('folded');
+			nso.addEventListener('transitionend', function() {
+				if (!nso.matches('.folded'))
+					nso.style['height'] = 'auto';
+			}, { once: true });
+			e.target.removeAttribute('disabled');
+		});
+	}
 }
